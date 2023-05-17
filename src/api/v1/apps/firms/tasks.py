b88d532323b8e7216a6656003a9ef2b8ab47726d
yuldashev222 <oybekyuldashov54@gmail.com>
2023-05-17 12:41:31 +0500

    asdasd

--- src/api/v1/apps/firms/tasks.py
+++ src/api/v1/apps/firms/tasks.py
@@ -9,13 +9,14 @@ from .services import EskizUz
 
 @shared_task
 def send_sms_to_director():
-    print('Hello ----------------------------')
-    print('Hello ++++++++++++++++++++++++++++')
     incomes = FirmIncome.objects.filter(deadline_date__isnull=False, is_paid=False)
     today_date = date.today()
     for income in incomes:
         if income.deadline_date - timedelta(days=1) == today_date:
             director = CustomUser.objects.get(id=income.creator.director_id)
-            message = f'Hurmatli {str(director)} "{str(income.to_pharmacy)}" "{str(income.from_firm)}" ' \
-                      f'{income.remaining_debt} ({EskizUz.get_str_price(income.remaining_debt)})'
+            message = f'Eslatma: "{str(income.from_firm)}" MCHJ tomonidan ' \
+                      f'{income.created_at.strftime("%d.%m.%Y")} kuni olingan ' \
+                      f'{income.price} so\'m tovardan qarzingiz ' \
+                      f'{income.price - income.remaining_debt} so\'m qoldi. ' \
+                      f'Qarzni to\'liq qaytarish muddatiga 3 kun qoldi.'
             EskizUz.send_sms(phone_number=director.phone_number, message=message)
