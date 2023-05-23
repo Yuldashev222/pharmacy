19f9aab7c91eb74a20fe43b638fc7d90da0b6e31
yuldashev222 <oybekyuldashov54@gmail.com>
2023-05-23 20:38:31 +0500

    asdasdasdasdasd

---
+++ src/api/v1/apps/receipts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from api.v1.apps.pharmacies.models import Pharmacy
+
+
+class Receipt(models.Model):
+    price = models.IntegerField(default=0)
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+    report_date = models.DateField()
+    shift = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ['pharmacy', 'shift', 'pharmacy']
+
+    def __str__(self):
+        return str(self.price)
