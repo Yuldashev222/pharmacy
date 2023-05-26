51d6687b041e6071dec9a218a73b4bc215edf5bb
yuldashev222 <oybekyuldashov54@gmail.com>
2023-05-26 09:31:08 +0500

    add receipt add

--- src/api/v1/apps/companies/views.py
+++ src/api/v1/apps/companies/views.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from django.db.models import Q
 from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -46,11 +47,12 @@ def company_details(request, *args, **kwargs):
             director_id=user.director_id, is_user_expense=True).values('id', 'name').order_by('-id'),
         'pharmacy_expense_types': ExpenseType.objects.filter(
             director_id=user.director_id, is_user_expense=False).values('id', 'name').order_by('-id'),
-        'employees': CustomUser.objects.filter(
-            director_id=user.director_id).values('id', 'first_name', 'last_name', 'role').order_by('-id'),
         'firms': Firm.objects.filter(director_id=user.director_id).values('id', 'name').order_by('-id')
     }
     if user.is_worker:
+        data['employees'] = CustomUser.objects.filter(
+            Q(director_id=user.director_id) & Q(pharmacy__isnull=True) | Q(pharmacy_id=user.pharmacy_id)
+        ).values('id', 'first_name', 'last_name', 'role').order_by('-id')
         data['remainder'] = get_remainder(report_date=str(date.today()), shift=user.shift, pharmacy_id=user.pharmacy_id)
         data['pharmacies'] = Pharmacy.objects.filter(id=user.pharmacy_id).values('id', 'name').order_by('-id')
 
@@ -63,6 +65,11 @@ def company_details(request, *args, **kwargs):
         report_date = request.query_params.get('report_date')
         shift = request.query_params.get('shift')
         pharmacy_id = request.query_params.get('pharmacy_id')
+
+        if pharmacy_id:
+            data['employees'] = CustomUser.objects.filter(
+                Q(director_id=user.director_id) & Q(pharmacy__isnull=True) | Q(pharmacy_id=pharmacy_id)
+            ).values('id', 'first_name', 'last_name', 'role').order_by('-id')
 
         if report_date and shift and pharmacy_id:
             data['remainder'] = get_remainder(report_date=report_date, shift=shift, pharmacy_id=pharmacy_id)
