3717f11a524b717c8e4733b464def46046511500
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-02 10:32:47 +0500

    add offers

--- src/api/v1/apps/offers/serializers.py
+++ src/api/v1/apps/offers/serializers.py
@@ -17,6 +17,7 @@ class OfferUpdateSerializer(serializers.ModelSerializer):
 
 class OfferRetrieveSerializer(serializers.ModelSerializer):
     status = serializers.CharField(source='get_status_display')
+    creator_phone_number = serializers.CharField(source='creator.phone_number')
     creator_name = serializers.StringRelatedField(source='creator')
     creator_role = serializers.CharField(source='creator.get_role_display')
 
