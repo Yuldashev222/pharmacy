6de6da4944a00411ea9ba528086a18f018abaed5
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-08 23:37:45 +0500

    add excel renderer

--- src/api/v1/apps/receipts/signals.py
+++ src/api/v1/apps/receipts/signals.py
@@ -9,14 +9,11 @@ from .models import Receipt
 
 @receiver(post_delete, sender=Receipt)
 def update_report(instance, *args, **kwargs):
-    price = Receipt.objects.filter(
-        report_date=instance.report_date,
-        pharmacy_id=instance.pharmacy_id
-    ).aggregate(s=Sum('price'))['s']
-    obj = PharmacyIncomeReportDay.objects.get_or_create(
-        pharmacy_id=instance.pharmacy_id,
-        report_date=instance.report_date,
-        director_id=instance.creator.director_id
-    )[0]
+    price = Receipt.objects.filter(report_date=instance.report_date,
+                                   pharmacy_id=instance.pharmacy_id
+                                   ).aggregate(s=Sum('price'))['s']
+
+    obj, _ = PharmacyIncomeReportDay.objects.get_or_create(pharmacy_id=instance.pharmacy_id,
+                                                           report_date=instance.report_date)
     obj.receipt_price = price
     obj.save()
