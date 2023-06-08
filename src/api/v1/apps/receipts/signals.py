91ff05753bf3973528d6e4ae436b4ab361ae5175
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-08 23:35:39 +0500

    add excel renderer

--- src/api/v1/apps/receipts/signals.py
+++ src/api/v1/apps/receipts/signals.py
@@ -14,7 +14,7 @@ def update_report(instance, *args, **kwargs):
         pharmacy_id=instance.pharmacy_id
     ).aggregate(s=Sum('price'))['s']
     obj = PharmacyIncomeReportDay.objects.get_or_create(
-        pharmacy_id=instance.to_pharmacy_id,
+        pharmacy_id=instance.pharmacy_id,
         report_date=instance.report_date,
         director_id=instance.creator.director_id
     )[0]
