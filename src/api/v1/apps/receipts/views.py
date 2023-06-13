ff9a5060aa0c170549bd5b1c93bf0a62f462b75a
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-13 15:27:01 +0500

    add excel renderer

--- src/api/v1/apps/receipts/views.py
+++ src/api/v1/apps/receipts/views.py
@@ -1,5 +1,4 @@
-from rest_framework.mixins import UpdateModelMixin, CreateModelMixin, RetrieveModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -10,10 +9,7 @@ from .models import Receipt
 from .serializers import WorkerReceiptCreateUpdateSerializer, DirectorManagerReceiptCreateUpdateSerializer
 
 
-class ReceiptCreateUpdateAPIView(CreateModelMixin,
-                                 RetrieveModelMixin,
-                                 UpdateModelMixin,
-                                 GenericViewSet):
+class ReceiptCreateUpdateAPIView(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['report_date', 'shift', 'pharmacy']
     permission_classes = [IsAuthenticated, NotProjectOwner]
