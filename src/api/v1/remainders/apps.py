ce652076d3d805eddc29b73143954ce38c0c85e1
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-18 17:10:10 +0500

    add owner page

--- src/api/v1/remainders/apps.py
+++ src/api/v1/remainders/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RemaindersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.v1.remainders'
+
+    def ready(self):
+        from . import signals
