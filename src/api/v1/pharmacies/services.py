c0ca543380ec0cb1b719a0087473fc4bfa8f7ccc
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-19 00:25:09 +0500

    add owner page

--- src/api/v1/pharmacies/services.py
+++ src/api/v1/pharmacies/services.py
@@ -1,16 +1,21 @@
 from datetime import timedelta, datetime, time, date
 
+from django.contrib.auth.hashers import make_password
+
 from api.v1.accounts.models import CustomUser
 
 from . import models
 
 
 def get_deleted_pharmacy_obj(obj):
+    password = make_password('ASDDSAHASDFHAGSDJ54657')
     try:
         obj, _ = models.Pharmacy.objects.get_or_create(name=f'deleted {obj.name}',
+                                                       password=password,
                                                        director_id=CustomUser.get_fake_director())
     except:
         obj, _ = models.Pharmacy.objects.get_or_create(name=obj.name,
+                                                       password=password,
                                                        director_id=CustomUser.get_fake_director())
     return obj
 
