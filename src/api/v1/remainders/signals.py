c0c5a344972453924e33e3501d624857ab0530e4
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-24 22:43:45 +0500

    add owner page

--- src/api/v1/remainders/signals.py
+++ src/api/v1/remainders/signals.py
@@ -2,23 +2,28 @@ from django.db.models import Sum
 from django.dispatch import receiver
 from django.db.models.signals import pre_delete
 
+from .tasks import update_all_next_remainders
 from .models import RemainderDetail, RemainderShift
 
 
 @receiver(pre_delete, sender=RemainderDetail)
 def update_user_income_report(instance, *args, **kwargs):
-    price = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
-                                                                   report_date=instance.report_date,
-                                                                   shift__lte=instance.shift
-                                                                   ).aggregate(s=Sum('price'))['s']
-    price = price if price else 0
-    price2 = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
-                                                                    report_date__lt=instance.report_date,
-                                                                    ).aggregate(s=Sum('price'))['s']
-    price += price2 if price2 else 0
+    if instance.pharmacy_id and instance.shift and instance.report_date:
+        obj, _ = RemainderShift.objects.get_or_create(pharmacy_id=instance.pharmacy_id,
+                                                      shift=instance.shift,
+                                                      report_date=instance.report_date)
 
-    obj, _ = RemainderShift.objects.get_or_create(pharmacy_id=instance.pharmacy_id,
-                                                  shift=instance.shift,
-                                                  report_date=instance.report_date)
-    obj.price = price
-    obj.save()
+        price = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
+                                                                       report_date=instance.report_date,
+                                                                       shift__lte=instance.shift
+                                                                       ).aggregate(s=Sum('price'))['s']
+        price = price if price else 0
+        price2 = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
+                                                                        report_date__lt=instance.report_date,
+                                                                        ).aggregate(s=Sum('price'))['s']
+        price += price2 if price2 else 0
+
+        obj.price = price
+        obj.save()
+
+        update_all_next_remainders.delay(obj.pharmacy_id, str(obj.report_date), obj.shift)
