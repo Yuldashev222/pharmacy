f3b6d35331ae4fe13259394f163a61a993fb515b
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-25 23:12:48 +0500

    add owner page

--- src/api/v1/pharmacies/signals.py
+++ src/api/v1/pharmacies/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from django.db.models.signals import pre_delete
 
 from api.v1.firms.models import FirmExpense, FirmDebtByMonth, FirmReport
+from api.v1.remainders.models import RemainderDetail
 
 from .models import Pharmacy
 
@@ -12,3 +13,4 @@ def update_connections(instance, *args, **kwargs):
     FirmExpense.objects.filter(from_pharmacy_id=instance.id).update(from_pharmacy_id=obj.id)
     FirmDebtByMonth.objects.filter(pharmacy_id=instance.id).update(pharmacy_id=obj.id)
     FirmReport.objects.filter(pharmacy_id=instance.id).update(pharmacy_id=obj.id)
+    RemainderDetail.objects.filter(pharmacy_id=instance.id).delete()
