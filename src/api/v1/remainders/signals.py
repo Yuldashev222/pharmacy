5df4cc0a2c13ce1e34952b5cd25bd98a72f72024
yuldashev222 <oybekyuldashov54@gmail.com>
2023-06-25 12:14:11 +0500

    add owner page

--- src/api/v1/remainders/signals.py
+++ src/api/v1/remainders/signals.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db.models import Sum
 from django.dispatch import receiver
 from django.db.models.signals import pre_delete
@@ -8,22 +10,41 @@ from .models import RemainderDetail, RemainderShift
 
 @receiver(pre_delete, sender=RemainderDetail)
 def update_user_income_report(instance, *args, **kwargs):
-    if instance.pharmacy_id and instance.shift and instance.report_date:
+    if instance.report_date and instance.shift and instance.pharmacy:
         obj, _ = RemainderShift.objects.get_or_create(pharmacy_id=instance.pharmacy_id,
                                                       shift=instance.shift,
                                                       report_date=instance.report_date)
 
-        price = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
-                                                                       report_date=instance.report_date,
-                                                                       shift__lte=instance.shift
-                                                                       ).aggregate(s=Sum('price'))['s']
-        price = price if price else 0
-        price2 = RemainderDetail.objects.exclude(id=instance.id).filter(pharmacy_id=instance.pharmacy_id,
-                                                                        report_date__lt=instance.report_date,
-                                                                        ).aggregate(s=Sum('price'))['s']
-        price += price2 if price2 else 0
+        if RemainderShift.objects.filter(id__lt=obj.id, pharmacy_id=obj.pharmacy_id).exists():
+            report_date = obj.report_date
+            if obj.shift > 1:
+                shift = obj.shift - 1
+            else:
+                shift = 3
+                report_date = obj.report_date - timedelta(days=1)
 
-        obj.price = price
+            old_obj_price = RemainderShift.get_price(obj.pharmacy_id, report_date, shift)
+            price = RemainderDetail.objects.filter(pharmacy_id=obj.pharmacy_id,
+                                                   report_date=obj.report_date,
+                                                   shift=obj.shift
+                                                   ).aggregate(s=Sum('price'))['s']
+
+            obj.price = price + old_obj_price
+
+        else:
+
+            price = RemainderDetail.objects.filter(pharmacy_id=obj.pharmacy_id,
+                                                   report_date=obj.report_date,
+                                                   shift__lte=obj.shift
+                                                   ).aggregate(s=Sum('price'))['s']
+            price = price if price else 0
+
+            price2 = RemainderDetail.objects.filter(pharmacy_id=obj.pharmacy_id,
+                                                    report_date__lt=obj.report_date,
+                                                    ).aggregate(s=Sum('price'))['s']
+            price += price2 if price2 else 0
+
+            obj.price = price
         obj.save()
 
-        update_all_next_remainders.delay(obj.pharmacy_id, str(obj.report_date), obj.shift)
+        update_all_next_remainders.delay(obj.pharmacy_id, str(obj.report_date), obj.shift, obj.price)
