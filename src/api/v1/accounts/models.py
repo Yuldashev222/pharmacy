001f36d0df9d0a9cf9e47a6e0ab847a95a5dc9fb
yuldashev222 <oybekyuldashov54@gmail.com>
2023-07-05 09:57:24 +0500

    change gitignore file

--- src/api/v1/accounts/models.py
+++ src/api/v1/accounts/models.py
@@ -44,8 +44,8 @@ class CustomUser(AbstractUser):
         return self.get_full_name()
 
     def save(self, *args, **kwargs):
-        self.first_name = text_normalize(self.first_name).title()
-        self.last_name = text_normalize(self.last_name).title()
+        self.first_name = text_normalize(self.first_name)
+        self.last_name = text_normalize(self.last_name)
         self.short_name = self.first_name + ' ' + '.'.join(
             list(map(lambda x: x[0], self.last_name.split()[:2]))).title()
         self.bio = text_normalize(self.bio)
